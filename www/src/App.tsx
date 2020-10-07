b839b9c2e2ecbbe984580c98a6700760f822d813
Agnieszka Olszewska <agnieszka.olszewska@brightinventions.pl>
2020-10-07 11:55:24 +0200

    Add polkadot api to www

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,38 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createRef, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { Container, Dimmer, Loader, Grid, Sticky, Message } from 'semantic-ui-react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ROUTE_ROOT } from './routes';
+import { useSubstrate, SubstrateContextProvider } from './substrate-lib';
+import { DeveloperConsole } from './substrate-lib/components';
+import { Dimmer, Loader, Grid, Message } from 'semantic-ui-react';
+import Home from './home/Home';
 
-function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Treasury app
-        </p>
-      </header>
-    </div>
-  );
-}
 
 function AppRoutes() {
   return (
     <Router>
       <Switch>
-        <Route exact={true} path={ROUTE_ROOT} component={Home} />
+        <Route exact={true} path={ROUTE_ROOT} component={Main} />
       </Switch>
     </Router>
   )
 }
 
+function Main() {
+  const [accountAddress, setAccountAddress] = useState(null);
+  const { apiState, keyring, keyringState, apiError } = useSubstrate();
+  const accountPair =
+    accountAddress &&
+    keyringState === 'READY' &&
+    keyring.getPair(accountAddress);
+
+    const loader = (text: any) =>
+    <Dimmer active>
+      <Loader size='small'>{text}</Loader>
+    </Dimmer>;
+
+  const message = (err: any) =>
+    <Grid centered columns={2} padded>
+      <Grid.Column>
+        <Message negative compact floating
+          header='Error Connecting to Substrate'
+          content={`${err}`}
+        />
+      </Grid.Column>
+    </Grid>;
+
+
+  if (apiState === 'ERROR') return message(apiError);
+  else if (apiState !== 'READY') return loader('Connecting to Substrate');
+
+  if (keyringState !== 'READY') {
+    return loader('Loading accounts (please review any extension\'s authorization)');
+  }
+
+  const contextRef = createRef<HTMLDivElement>();
+
+  return (
+    <div ref={contextRef}>
+      <Home/>
+      <DeveloperConsole />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="main">
+    <SubstrateContextProvider>
       <AppRoutes />
-    </div>
+    </SubstrateContextProvider>
   )
 }
 
