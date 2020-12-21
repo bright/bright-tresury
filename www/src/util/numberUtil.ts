faf980a4b05bc45336d4c6fed5acfaf5341b89c0
Szymon Miloch <szymon.miloch@brightinventions.pl>
2020-12-21 16:54:07 +0100

    TREAS-25 Changed format number method to use `toLocaleString` instead of Regex

--- www/src/util/numberUtil.ts
+++ www/src/util/numberUtil.ts
@@ -3,5 +3,5 @@
  * @param value
  */
 export function formatNumber(value: number): string {
-    return value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    return value.toLocaleString()
 }
