5548e6c4fae1435e13e83aba611d67b3737c00e4
Szymon Miloch <szymon.miloch@brightinventions.pl>
2021-01-02 11:11:03 +0100

    TREAS-13 Nav bar UI for tablet

--- www/src/main/Menu.tsx
+++ www/src/main/Menu.tsx
@@ -14,21 +14,22 @@ import {makeStyles} from "@material-ui/core/styles";
 import pathToRegexp from "path-to-regexp"
 import {breakpoints} from "../theme/theme";
 import {WithWidthProps} from "@material-ui/core/withWidth/withWidth";
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme: Theme) => {
         const drawerWidth = 250
+        const miniDrawerWidth = 80
         return createStyles({
             root: {
-                [theme.breakpoints.up(breakpoints.mobile)]: {
-                    width: drawerWidth,
-                    flexShrink: 0,
-                }
-            },
-            drawer: {
-                width: drawerWidth
+                width: drawerWidth,
+                flexShrink: 0,
             },
             item: {
+                display: 'flex',
                 padding: '20px 38px',
+                [theme.breakpoints.down(breakpoints.tablet)]: {
+                    padding: '28px',
+                },
             },
             text: {
                 fontWeight: 500
@@ -38,10 +39,27 @@ const useStyles = makeStyles((theme: Theme) => {
             },
             icon: {
                 maxHeight: 28,
+                width: 22,
                 [theme.breakpoints.down(breakpoints.tablet)]: {
-                    maxHeight: 24,
+                    maxHeight: 28,
+                    width: 26
                 }
-            }
+            },
+            drawerOpen: {
+                width: drawerWidth,
+                transition: theme.transitions.create("width", {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.enteringScreen
+                })
+            },
+            drawerClose: {
+                transition: theme.transitions.create("width", {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen
+                }),
+                overflowX: "hidden",
+                width: miniDrawerWidth,
+            },
         })
     }
 )
@@ -53,7 +71,8 @@ const Menu: React.FC<WithWidthProps> = ({width}) => {
     const {t} = useTranslation()
 
     const isMobile = () => isWidthDown(breakpoints.mobile, width!)
-    const [drawerState, setDrawerState] = useState(isMobile() ? false : true)
+    const isTabletOrMobile = () => isWidthDown(breakpoints.tablet, width!)
+    const [drawerState, setDrawerState] = useState(!isTabletOrMobile())
 
     const matchesCurrentPath = (path: string): boolean => {
         const regexp = pathToRegexp(`${path}*`)
@@ -68,31 +87,41 @@ const Menu: React.FC<WithWidthProps> = ({width}) => {
     const container =
         window !== undefined ? () => window.document.body : undefined;
 
-    return <div className={classes.root}>
-        <Drawer
-            container={container}
-            onClose={() => setDrawerState(false)}
-            anchor={isMobile() ? 'top' : 'left'}
-            classes={{
-                paper: classes.drawer
-            }}
-            variant="permanent">
-            <div>
-                <List>
-                    {MENU_ITEMS.map(({translationKey, svg, path}, index) => (
-                        <ListItem className={classes.item} button key={index} onClick={() => goTo(path)}>
-                            <ListItemIcon><img className={classes.icon} src={svg}/></ListItemIcon>
-                            <ListItemText
-                                classes={{
-                                    primary: `${classes.text} ${matchesCurrentPath(path) ? classes.textSelected : ''}`
-                                }}
-                                primary={t(translationKey)}/>
-                        </ListItem>
-                    ))}
-                </List>
-            </div>
-        </Drawer>
-    </div>
+    return <Drawer
+        onTouchStart={() => {
+            if (isTabletOrMobile()) {
+                setDrawerState(!drawerState)
+            }
+        }}
+        container={container}
+        onClose={() => setDrawerState(false)}
+        anchor={isMobile() ? 'top' : 'left'}
+        className={clsx({
+            [classes.drawerOpen]: drawerState,
+            [classes.drawerClose]: !drawerState
+        })}
+        classes={{
+            paper: clsx({
+                [classes.drawerOpen]: drawerState,
+                [classes.drawerClose]: !drawerState
+            })
+        }}
+        variant="permanent">
+        <div>
+            <List>
+                {MENU_ITEMS.map(({translationKey, svg, path}, index) => (
+                    <ListItem className={classes.item} button key={index} onClick={() => goTo(path)}>
+                        <ListItemIcon><img className={classes.icon} src={svg}/></ListItemIcon>
+                        <ListItemText
+                            classes={{
+                                primary: `${classes.text} ${matchesCurrentPath(path) ? classes.textSelected : ''}`
+                            }}
+                            primary={t(translationKey)}/>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    </Drawer>
 }
 
 export default withWidth()(Menu)
