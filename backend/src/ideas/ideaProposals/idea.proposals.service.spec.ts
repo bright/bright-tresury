aa1c0561cf9281e021f8729f6b1e31f691a15a8a
Szymon Miloch <szymon.miloch@brightinventions.pl>
2021-01-29 18:14:04 +0100

    Fix tests by eager loading networks relation for idea in proposals service tests.

--- backend/src/ideas/ideaProposals/idea.proposals.service.spec.ts
+++ backend/src/ideas/ideaProposals/idea.proposals.service.spec.ts
@@ -1,17 +1,17 @@
-import { Test } from '@nestjs/testing';
-import { getRepositoryToken } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { BlockchainService } from '../../blockchain/blockchain.service';
-import { UpdateExtrinsicDto } from '../../extrinsics/dto/updateExtrinsic.dto';
-import { ExtrinsicEvent } from "../../extrinsics/extrinsicEvent";
-import { beforeAllSetup, cleanDatabase } from '../../utils/spec.helpers';
-import { Idea } from '../idea.entity';
-import { IdeaNetwork } from '../ideaNetwork.entity';
-import { IdeasModule } from "../ideas.module";
-import { IdeasService } from "../ideas.service";
-import { createIdea } from "../spec.helpers";
-import { CreateIdeaProposalDto, IdeaProposalDataDto } from "./dto/createIdeaProposal.dto";
-import { IdeaProposalsService } from "./idea.proposals.service";
+import {Test} from '@nestjs/testing';
+import {getRepositoryToken} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {BlockchainService} from '../../blockchain/blockchain.service';
+import {UpdateExtrinsicDto} from '../../extrinsics/dto/updateExtrinsic.dto';
+import {ExtrinsicEvent} from "../../extrinsics/extrinsicEvent";
+import {beforeAllSetup, cleanDatabase} from '../../utils/spec.helpers';
+import {Idea} from '../idea.entity';
+import {IdeaNetwork} from '../ideaNetwork.entity';
+import {IdeasModule} from "../ideas.module";
+import {IdeasService} from "../ideas.service";
+import {createIdea} from "../spec.helpers";
+import {CreateIdeaProposalDto, IdeaProposalDataDto} from "./dto/createIdeaProposal.dto";
+import {IdeaProposalsService} from "./idea.proposals.service";
 import {IdeaStatus} from "../ideaStatus";
 
 describe('IdeaProposalsService', () => {
@@ -57,10 +57,11 @@ describe('IdeaProposalsService', () => {
         await cleanDatabase()
 
         const partialIdea = {
-            networks: [{ name: 'local', value: 10 }],
+            networks: [{name: 'local', value: 10}],
             beneficiary: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
         }
-        idea = await createIdea(partialIdea, ideasService())
+        const createdIdea = await createIdea(partialIdea, ideasService())
+        idea = (await ideasService().findOne(createdIdea.id))!
         dto = new CreateIdeaProposalDto(idea.networks![0].id, '', '', new IdeaProposalDataDto(3))
     })
 
@@ -71,36 +72,34 @@ describe('IdeaProposalsService', () => {
     describe('createProposal', () => {
         it('should assign extrinsic to idea network', async () => {
             await service().createProposal(idea.id, dto)
-            const actual = await ideaNetworkRepository().findOne(idea.networks![0].id, { relations: ['extrinsic'] })
+            const actual = await ideaNetworkRepository().findOne(idea.networks![0].id, {relations: ['extrinsic']})
             expect(actual!.extrinsic).toBeTruthy()
         })
 
-        // TODO fix and uncomment!
-        // it('should run extractExtrinsic', async () => {
-        //     const spy = jest.spyOn(service(), 'extractEvents').mockImplementationOnce(async (events: ExtrinsicEvent[], network: IdeaNetwork) => {
-        //         return
-        //     })
-        //     await service().createProposal(idea.id, dto)
-        //
-        //     expect(spy).toHaveBeenCalled()
-        // })
+        it('should run extractExtrinsic', async () => {
+            const spy = jest.spyOn(service(), 'extractEvents').mockImplementationOnce(async (events: ExtrinsicEvent[], network: IdeaNetwork) => {
+                return
+            })
+            await service().createProposal(idea.id, dto)
+
+            expect(spy).toHaveBeenCalled()
+        })
     })
 
-    // TODO fix and uncomment!
-    // describe('extractExtrinsic', () => {
-    //     it('should assign blockchainProposalId to idea network', async () => {
-    //         await service().extractEvents(extrinsic.events, idea.networks![0])
-    //
-    //         const i = await ideaNetworkRepository()
-    //         const actual = await i.findOne(idea.networks![0].id)
-    //         expect(actual!.blockchainProposalId).toBe(proposalIndex)
-    //     })
-    //     it(`should change idea status to turned into proposal`, async () => {
-    //         await service().extractEvents(extrinsic.events, idea.networks![0])
-    //
-    //         const repository = await ideaRepository()
-    //         const actualIdea = await repository.findOne(idea.id)
-    //         expect(actualIdea!.status).toBe(IdeaStatus.TurnedIntoProposal)
-    //     })
-    // })
+    describe('extractExtrinsic', () => {
+        it('should assign blockchainProposalId to idea network', async () => {
+            await service().extractEvents(extrinsic.events, idea.networks![0])
+
+            const i = await ideaNetworkRepository()
+            const actual = await i.findOne(idea.networks![0].id)
+            expect(actual!.blockchainProposalId).toBe(proposalIndex)
+        })
+        it(`should change idea status to turned into proposal`, async () => {
+            await service().extractEvents(extrinsic.events, idea.networks![0])
+
+            const repository = await ideaRepository()
+            const actualIdea = await repository.findOne(idea.id)
+            expect(actualIdea!.status).toBe(IdeaStatus.TurnedIntoProposal)
+        })
+    })
 });
