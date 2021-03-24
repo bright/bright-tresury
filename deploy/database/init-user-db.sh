f48334875ac28005536fd1424f35e19aef5f4d30
Szymon Miloch <milszym@gmail.com>
2021-03-24 16:55:02 +0100

    TREAS-66 Add privileges to database web role so that it can access tables created by authorization_manager user

--- deploy/database/init-user-db.sh
+++ deploy/database/init-user-db.sh
@@ -3,21 +3,31 @@
 psql --echo-all --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE ROLE deployer WITH LOGIN SUPERUSER;
 CREATE ROLE web WITH LOGIN;
+CREATE ROLE authorization_manager WITH LOGIN PASSWORD 'password';
 
 CREATE DATABASE "treasury" WITH OWNER deployer;
 CREATE DATABASE "treasury_test" WITH OWNER deployer;
 CREATE DATABASE "treasury_authorization" WITH OWNER deployer;
 CREATE DATABASE "treasury_authorization_test" WITH OWNER deployer;
 
-CREATE USER authorization_manager with encrypted password 'password';
-GRANT ALL PRIVILEGES ON DATABASE "treasury_authorization" to authorization_manager;
-GRANT ALL PRIVILEGES ON DATABASE "treasury_authorization_test" to authorization_manager;
-
 EOSQL
 
-for db_name in treasury treasury_test treasury_authorization treasury_authorization_test; do
-    echo "Setup web role access for $db_name"
+for db_name in treasury_authorization treasury_authorization_test; do
+    echo "Setup access for $db_name"
 psql --echo-all --username "deployer" --dbname ${db_name} <<-EOSQL
+alter default privileges in schema public grant select, insert, update, delete, truncate on tables to authorization_manager;
+alter default privileges in schema public grant all on sequences to authorization_manager;
+
+alter default privileges for user authorization_manager in schema public grant select, insert, update, delete, truncate ON TABLES TO web;
+alter default privileges for user authorization_manager in schema public grant all on sequences to web;
+EOSQL
+done
+
+for db_name in treasury treasury_test; do
+    echo "Setup access for $db_name"
+psql --echo-all --username "deployer" --dbname ${db_name} <<-EOSQL
+grant all privileges on all tables in schema public to web;
+grant all privileges on all sequences in schema public to web;
 alter default privileges in schema public grant select, insert, update, delete, truncate on tables to web;
 alter default privileges in schema public grant all on sequences to web;
 EOSQL
