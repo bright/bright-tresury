54e78ad73cebe95a6af1c0c0b0b3a187cab44288
Szymon Miloch <milszym@gmail.com>
2021-04-23 17:07:48 +0200

    TREAS-67 Create useAccounts hook for retrieving available blockchain accounts

--- www/src/auth/sign-up/blockchain/BlockchainSignUp.tsx
+++ www/src/auth/sign-up/blockchain/BlockchainSignUp.tsx
@@ -1,14 +1,13 @@
 import {Formik} from "formik";
 import {FormikHelpers} from "formik/dist/types";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {useTranslation} from "react-i18next";
 import * as Yup from "yup";
 import Container from "../../../components/form/Container";
 import {LoadingState} from "../../../components/loading/LoadingWrapper";
 import {fullValidatorForSchema} from "../../../util/form.util";
-import {signUp} from "../../auth.api";
+import {blockchainSignUp} from "../../auth.api";
 import {useSuperTokensRequest} from "../../supertokens.utils/useSuperTokensRequest";
-import BlockchainSignUpSuccess from "./BlockchainSignUpSuccess";
 import {GetUserAgreementYupSchema, TermsAgreementCheckbox} from "../common/TermsAgreementCheckbox";
 import {PrivacyNotice} from "../common/PrivacyNotice";
 import {SignUpButton} from "../common/SignUpButton";
@@ -16,9 +15,8 @@ import {AlreadyLoggedIn} from "../common/AlreadyLoggedIn";
 import {SignUpInputWrapper} from "../common/SignUpInputWrapper";
 import {SignUpFormWrapper} from "../common/SignUpFormWrapper";
 import {AccountSelect} from "../../../components/select/AccountSelect";
-import {Account} from "../../../substrate-lib/components/SubmittingTransaction";
-import {KeyringState} from "../../../substrate-lib/SubstrateContext";
-import {useSubstrate} from "../../../substrate-lib";
+import {Account} from "../../../substrate-lib/hooks/useAccounts";
+import SignUpSuccess from "../common/SignUpSucces";
 
 interface BlockchainSignUpValues {
     account: Account,
@@ -28,18 +26,7 @@ interface BlockchainSignUpValues {
 const BlockchainSignUp: React.FC = () => {
     const {t} = useTranslation()
 
-    const {keyringState, keyring} = useSubstrate();
-    const [accounts, setAccounts] = useState<Account[]>([])
-    useEffect(() => {
-        if (keyringState === KeyringState.READY && keyring) {
-            const keyringAccounts = keyring.getAccounts().map((account) => {
-                return {name: account.meta?.name || '', address: account.address} as Account
-            })
-            setAccounts(keyringAccounts)
-        }
-    }, [keyring, keyringState])
-
-    const {call, loadingState} = useSuperTokensRequest(signUp)
+    const {call, loadingState} = useSuperTokensRequest(blockchainSignUp)
 
     const onSubmit = async (values: BlockchainSignUpValues, {setErrors}: FormikHelpers<BlockchainSignUpValues>) => {
         // TODO:// create blockchain sign up logic
@@ -51,7 +38,7 @@ const BlockchainSignUp: React.FC = () => {
 
     if (loadingState === LoadingState.Resolved) {
         return <Container title={t('auth.signup.title')}>
-            <BlockchainSignUpSuccess/>
+            <SignUpSuccess subtitle={t('auth.signup.blockchainSignUp.successSubtitle')}/>
         </Container>
     }
 
@@ -72,7 +59,7 @@ const BlockchainSignUp: React.FC = () => {
           }) =>
             <SignUpFormWrapper handleSubmit={handleSubmit}>
                 <SignUpInputWrapper>
-                    <AccountSelect account={values.account} accounts={accounts}/>
+                    <AccountSelect account={values.account}/>
                 </SignUpInputWrapper>
                 <SignUpInputWrapper>
                     <TermsAgreementCheckbox/>
