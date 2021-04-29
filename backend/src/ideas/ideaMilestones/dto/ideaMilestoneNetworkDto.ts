21ee477873ba6aa15e982cfeb2b6f4aa83611cfa
Sławomir Kopaczewski <slawekkopaczewski@gmail.com>
2021-04-29 12:58:19 +0200

    TREAS-8 Add extrinsic to idea milestone network dto

--- backend/src/ideas/ideaMilestones/dto/ideaMilestoneNetworkDto.ts
+++ backend/src/ideas/ideaMilestones/dto/ideaMilestoneNetworkDto.ts
@@ -1,6 +1,7 @@
 import {ApiProperty} from "@nestjs/swagger";
 import {IdeaMilestoneNetwork} from "../entities/idea.milestone.network.entity";
 import {IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID} from "class-validator";
+import {ExtrinsicDto, toExtrinsicDto} from "../../../extrinsics/dto/extrinsic.dto";
 
 export class IdeaMilestoneNetworkDto {
     @ApiProperty({
@@ -24,15 +25,24 @@ export class IdeaMilestoneNetworkDto {
     @IsNumber()
     value: number
 
-    constructor(name: string, value: number, id?: string) {
+    @IsOptional()
+    extrinsic: ExtrinsicDto | null
+
+    constructor(name: string, value: number, extrinsic: ExtrinsicDto | null, id?: string) {
         this.id = id
         this.name = name
         this.value = Number(value)
+        this.extrinsic = extrinsic
     }
 }
 
 export const mapIdeaMilestoneNetworkEntityToIdeaMilestoneNetworkDto = (
-    { id, name, value  }: IdeaMilestoneNetwork
+    { id, name, value, extrinsic  }: IdeaMilestoneNetwork
 ): IdeaMilestoneNetworkDto => {
-    return new IdeaMilestoneNetworkDto(name, value, id)
+    return new IdeaMilestoneNetworkDto(
+        name,
+        value,
+        extrinsic ? toExtrinsicDto(extrinsic) : null,
+        id
+    )
 }
