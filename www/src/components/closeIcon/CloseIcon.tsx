8336e445e8a26a8baa1a44d419a4bb4931db16e6
Szymon Miloch <milszym@gmail.com>
2021-05-29 13:27:06 +0200

    TREAS-135 Fix wrong close icon hover effect in idea and proposal details.

--- www/src/components/closeIcon/CloseIcon.tsx
+++ www/src/components/closeIcon/CloseIcon.tsx
@@ -1,24 +1,25 @@
-import React from "react";
-import {createStyles, makeStyles} from "@material-ui/core/styles";
-import {IconButton} from "../button/IconButton";
-import crossSvg from "../../assets/cross.svg";
-import {ClassNameProps} from "../props/className.props";
-import clsx from "clsx";
+import React from 'react'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import { IconButton } from '../button/IconButton'
+import crossSvg from '../../assets/cross.svg'
+import { ClassNameProps } from '../props/className.props'
+import clsx from 'clsx'
 
 const useStyles = makeStyles(() =>
     createStyles({
         root: {
             alignSelf: 'self-start',
-            padding: '0 0 2em 2em',
+            margin: '0 0 2em 2em',
         },
-    }))
+    }),
+)
 
 interface Props {
     onClose: () => void
 }
 
-export const CloseIcon: React.FC<Props & ClassNameProps> = ({onClose, className = ''}) => {
+export const CloseIcon: React.FC<Props & ClassNameProps> = ({ onClose, className = '' }) => {
     const classes = useStyles()
 
-    return <IconButton className={clsx(classes.root, className)} svg={crossSvg} onClick={onClose}/>
+    return <IconButton className={clsx(classes.root, className)} svg={crossSvg} onClick={onClose} />
 }
