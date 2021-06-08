12c3ecf3cb2fffdde34ce95ee04db64e46840c35
Sławomir Kopaczewski <slawekkopaczewski@gmail.com>
2021-06-08 14:36:23 +0200

    TREAS-150 Remove unused top bar account menu items

--- www/src/main/top-bar/account/AccountInfo.tsx
+++ www/src/main/top-bar/account/AccountInfo.tsx
@@ -1,17 +1,17 @@
-import {Divider} from "@material-ui/core";
-import Menu from "@material-ui/core/Menu";
-import {createStyles, makeStyles} from "@material-ui/core/styles";
-import React from 'react';
-import {useTranslation} from "react-i18next";
-import {useHistory} from "react-router-dom";
-import arrowSvg from "../../../assets/account_menu_arrow.svg";
-import {useAuth} from "../../../auth/AuthContext";
-import {IconButton} from "../../../components/button/IconButton";
-import {ROUTE_ACCOUNT} from "../../../routes/routes";
-import AccountImage from "./AccountImage";
-import EmailVerifyErrorMenuItem from "./EmailVerifyErrorMenuItem";
-import MenuItem from "./MenuItem";
-import SignOutMenuItem from "./SignOutMenuItem";
+import { Divider } from '@material-ui/core'
+import Menu from '@material-ui/core/Menu'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router-dom'
+import arrowSvg from '../../../assets/account_menu_arrow.svg'
+import { useAuth } from '../../../auth/AuthContext'
+import { IconButton } from '../../../components/button/IconButton'
+import { ROUTE_ACCOUNT } from '../../../routes/routes'
+import AccountImage from './AccountImage'
+import EmailVerifyErrorMenuItem from './EmailVerifyErrorMenuItem'
+import MenuItem from './MenuItem'
+import SignOutMenuItem from './SignOutMenuItem'
 
 const useStyles = makeStyles(() =>
     createStyles({
@@ -22,10 +22,10 @@ const useStyles = makeStyles(() =>
 )
 
 const AccountInfo = () => {
-    const {t} = useTranslation()
+    const { t } = useTranslation()
     const classes = useStyles()
     const history = useHistory()
-    const {user} = useAuth()
+    const { user } = useAuth()
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
 
@@ -44,25 +44,18 @@ const AccountInfo = () => {
 
     return (
         <div className={classes.root}>
-            <AccountImage/>
-            <IconButton onClick={handleClick} alt={t('topBar.showAccountMenu')} svg={arrowSvg}/>
-            <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-            >
-                {user && !user.isEmailVerified ? <>
-                    <EmailVerifyErrorMenuItem/>
-                    <Divider/>
-                </> : null}
+            <AccountImage />
+            <IconButton onClick={handleClick} alt={t('topBar.showAccountMenu')} svg={arrowSvg} />
+            <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                {user && !user.isEmailVerified ? (
+                    <>
+                        <EmailVerifyErrorMenuItem />
+                        <Divider />
+                    </>
+                ) : null}
                 <MenuItem onClick={goToAccount}>{t('topBar.account.account')}</MenuItem>
-                <MenuItem disabled={true}>{t('topBar.account.ideas')}</MenuItem>
-                <MenuItem disabled={true}>{t('topBar.account.proposals')}</MenuItem>
-                <MenuItem disabled={true}>{t('topBar.account.bounties')}</MenuItem>
-                <Divider/>
-                <SignOutMenuItem/>
+                <Divider />
+                <SignOutMenuItem />
             </Menu>
         </div>
     )
