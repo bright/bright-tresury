515532fcd80909d42fda64f00550cc029ed0bdc1
Robert Koprowski <robert.koprowski@brightinventions.pl>
2021-12-28 17:20:20 +0100

    TREAS-317 fix stats.currentSpendPeriod

--- www/src/stats/StatsHeader.tsx
+++ www/src/stats/StatsHeader.tsx
@@ -77,7 +77,7 @@ const StatsHeader = ({ timeLeft }: StatsHeaderProps) => {
                 </Button>
                 <TimeSelectWrapper className={classes.timeSelectWrapper}>
                     <p className={classes.timeInfo}>
-                        {t(`status.currentSpendPeriod`)}:{' '}
+                        {t(`stats.currentSpendPeriod`)}:{' '}
                         <span className={classes.timeInfoBold}>{timeLeftLabel}</span>
                     </p>
                 </TimeSelectWrapper>
