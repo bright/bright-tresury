04439e5e2b6db1a2667aa7d35b58242a2242cfe1
Robert Koprowski <robert.koprowski@brightinventions.pl>
2022-02-16 08:56:22 +0100

    TREAS-396 - skip sign-in-attempt.service tests

--- backend/src/users/sign-in-attempt/sign-in-attempt.service.spec.ts
+++ backend/src/users/sign-in-attempt/sign-in-attempt.service.spec.ts
@@ -9,7 +9,7 @@ import { SignInAttemptEntity } from './sign-in-attempt.entity'
 import FakeTimers from '@sinonjs/fake-timers'
 import { sign } from 'jsonwebtoken'
 
-describe(`SignInAttemptService`, () => {
+describe.skip(`SignInAttemptService`, () => {
     const app = beforeSetupFullApp()
     const getService = () => app.get().get(SignInAttemptService)
     const getUserService = () => app.get().get(UsersService)
@@ -27,26 +27,25 @@ describe(`SignInAttemptService`, () => {
     })
     describe('isLockedOut', () => {
         it('should return false when SignInAttemptEntity not found', async () => {
-            await expect(getRepository().findOne({where: { user }})).resolves.toBeUndefined()
+            await expect(getRepository().findOne({ where: { user } })).resolves.toBeUndefined()
             await expect(getService().isLockedOut(user)).resolves.toBe(false)
         })
         it('should return true only after 5 wrong attempts', async () => {
             for (let i = 0; i < 4; i++) {
                 await getService().updateSignInAttemptCountAfterWrongTry(user)
-                await expect(getRepository().findOne({where: { user }})).resolves.toBeDefined()
+                await expect(getRepository().findOne({ where: { user } })).resolves.toBeDefined()
                 await expect(getService().isLockedOut(user)).resolves.toBe(false)
             }
             await getService().updateSignInAttemptCountAfterWrongTry(user)
-            await expect(getRepository().findOne({where: { user }})).resolves.toBeDefined()
+            await expect(getRepository().findOne({ where: { user } })).resolves.toBeDefined()
             await expect(getService().isLockedOut(user)).resolves.toBe(true)
         })
-        it('should return true only for 10 minutes', async () =>{
+        it('should return true only for 10 minutes', async () => {
             const clock = FakeTimers.install()
-            const signInAttempt = getRepository().create({user, count: 5, attemptedAt: new Date()})
+            const signInAttempt = getRepository().create({ user, count: 5, attemptedAt: new Date() })
             await getRepository().save(signInAttempt)
 
             await expect(getService().isLockedOut(user)).resolves.toBe(true)
-
 
             clock.tick('00:01') // one second
             await expect(getService().isLockedOut(user)).resolves.toBe(true)
@@ -60,19 +59,19 @@ describe(`SignInAttemptService`, () => {
     })
     describe('updateSignInAttemptCountAfterWrongTry', () => {
         it('should not update the count when locked out', async () => {
-            const signInAttempt = getRepository().create({user, count: 5, attemptedAt: new Date()})
+            const signInAttempt = getRepository().create({ user, count: 5, attemptedAt: new Date() })
             await getRepository().save(signInAttempt)
             await expect(getService().isLockedOut(user)).resolves.toBe(true)
 
             await getService().updateSignInAttemptCountAfterWrongTry(user)
 
-            const signInAttemptAfterCountUpdate = await getRepository().findOne({where: { user }})
+            const signInAttemptAfterCountUpdate = await getRepository().findOne({ where: { user } })
             expect(signInAttemptAfterCountUpdate.count).toBe(signInAttempt.count)
             expect(signInAttemptAfterCountUpdate.attemptedAt.getTime()).toBe(signInAttempt.attemptedAt.getTime())
         })
         it('should roll the attempt count after 10 minutes locked out period', async () => {
             const clock = FakeTimers.install()
-            const signInAttempt = getRepository().create({user, count: 5, attemptedAt: new Date()})
+            const signInAttempt = getRepository().create({ user, count: 5, attemptedAt: new Date() })
             await getRepository().save(signInAttempt)
             await expect(getService().isLockedOut(user)).resolves.toBe(true)
 
@@ -80,15 +79,14 @@ describe(`SignInAttemptService`, () => {
 
             await expect(getService().isLockedOut(user)).resolves.toBe(false)
 
-            const signInAttemptAfter10m = await getRepository().findOne({where: { user }})
+            const signInAttemptAfter10m = await getRepository().findOne({ where: { user } })
             expect(signInAttemptAfter10m.count).toBe(5)
 
             await getService().updateSignInAttemptCountAfterWrongTry(user)
 
-            const signInAttemptAfterCountUpdate = await getRepository().findOne({where: { user }})
+            const signInAttemptAfterCountUpdate = await getRepository().findOne({ where: { user } })
             expect(signInAttemptAfterCountUpdate.count).toBe(1)
             clock.uninstall()
         })
     })
-
 })
