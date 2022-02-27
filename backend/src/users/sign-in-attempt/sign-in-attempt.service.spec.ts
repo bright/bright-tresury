364574e1a218b7ca9d3db1f8a4b579ae01ad6e61
Robert Koprowski <robert.koprowski@brightinventions.pl>
2022-02-27 20:32:53 +0100

    TREAS-392 organize sign-in-attempt tests into tripple A

--- backend/src/users/sign-in-attempt/sign-in-attempt.service.spec.ts
+++ backend/src/users/sign-in-attempt/sign-in-attempt.service.spec.ts
@@ -1,67 +1,82 @@
 import { beforeSetupFullApp, cleanDatabase } from '../../utils/spec.helpers'
 import { UsersService } from '../users.service'
 import { getRepositoryToken } from '@nestjs/typeorm'
-import { UserEntity } from '../user.entity'
+
 import { v4 as uuid } from 'uuid'
-import { CreateUserDto } from '../dto/create-user.dto'
+
 import { SignInAttemptService } from './sign-in-attempt.service'
 import { SignInAttemptEntity } from './sign-in-attempt.entity'
 import FakeTimers from '@sinonjs/fake-timers'
-import { sign } from 'jsonwebtoken'
 
-describe.skip(`SignInAttemptService`, () => {
+describe(`SignInAttemptService`, () => {
     const app = beforeSetupFullApp()
     const getService = () => app.get().get(SignInAttemptService)
     const getUserService = () => app.get().get(UsersService)
     const getRepository = () => app.get().get(getRepositoryToken(SignInAttemptEntity))
 
-    let user: UserEntity
+    const setUpUser = async ({ authId, username, email }: { authId?: string; username?: string; email?: string }) =>
+        getUserService().create({
+            authId: authId ?? uuid(),
+            username: username ?? 'Bob',
+            email: email ?? 'bob@email.com',
+        })
 
     beforeEach(async () => {
         await cleanDatabase()
-        user = await getUserService().create({
-            authId: uuid(),
-            username: 'Bob',
-            email: 'bob@email.com',
-        } as CreateUserDto)
     })
     describe('isLockedOut', () => {
         it('should return false when SignInAttemptEntity not found', async () => {
-            await expect(getRepository().findOne({ where: { user } })).resolves.toBeUndefined()
+            const user = await setUpUser({})
             await expect(getService().isLockedOut(user)).resolves.toBe(false)
         })
-        it('should return true only after 5 wrong attempts', async () => {
+
+        it('should return false after 4 wrong attempts', async () => {
+            const user = await setUpUser({})
             for (let i = 0; i < 4; i++) {
                 await getService().updateSignInAttemptCountAfterWrongTry(user)
-                await expect(getRepository().findOne({ where: { user } })).resolves.toBeDefined()
-                await expect(getService().isLockedOut(user)).resolves.toBe(false)
             }
-            await getService().updateSignInAttemptCountAfterWrongTry(user)
+            await expect(getRepository().findOne({ where: { user } })).resolves.toBeDefined()
+            await expect(getService().isLockedOut(user)).resolves.toBe(false)
+        })
+
+        it('should return true after 5 wrong attempt', async () => {
+            const user = await setUpUser({})
+            for (let i = 0; i < 5; i++) {
+                await getService().updateSignInAttemptCountAfterWrongTry(user)
+            }
             await expect(getRepository().findOne({ where: { user } })).resolves.toBeDefined()
             await expect(getService().isLockedOut(user)).resolves.toBe(true)
         })
-        it('should return true only for 10 minutes', async () => {
+
+        it('should return false after 10 minutes', async () => {
             const clock = FakeTimers.install()
+            const user = await setUpUser({})
             const signInAttempt = getRepository().create({ user, count: 5, attemptedAt: new Date() })
             await getRepository().save(signInAttempt)
 
-            await expect(getService().isLockedOut(user)).resolves.toBe(true)
+            clock.tick('10:01') // ten minutes
 
-            clock.tick('00:01') // one second
-            await expect(getService().isLockedOut(user)).resolves.toBe(true)
-            clock.tick('05:00') // five minutes
-            await expect(getService().isLockedOut(user)).resolves.toBe(true)
-            clock.tick('05:00') // five minutes (10 minutes 1 seconds after start)
             await expect(getService().isLockedOut(user)).resolves.toBe(false)
+            clock.uninstall()
+        })
 
+        it('should return true after 9 minutes 59 seconds', async () => {
+            const clock = FakeTimers.install()
+            const user = await setUpUser({})
+            const signInAttempt = getRepository().create({ user, count: 5, attemptedAt: new Date() })
+            await getRepository().save(signInAttempt)
+
+            clock.tick('09:59') // almost ten minutes
+
+            await expect(getService().isLockedOut(user)).resolves.toBe(true)
             clock.uninstall()
         })
     })
     describe('updateSignInAttemptCountAfterWrongTry', () => {
         it('should not update the count when locked out', async () => {
+            const user = await setUpUser({})
             const signInAttempt = getRepository().create({ user, count: 5, attemptedAt: new Date() })
             await getRepository().save(signInAttempt)
-            await expect(getService().isLockedOut(user)).resolves.toBe(true)
 
             await getService().updateSignInAttemptCountAfterWrongTry(user)
 
@@ -69,23 +84,20 @@ describe.skip(`SignInAttemptService`, () => {
             expect(signInAttemptAfterCountUpdate.count).toBe(signInAttempt.count)
             expect(signInAttemptAfterCountUpdate.attemptedAt.getTime()).toBe(signInAttempt.attemptedAt.getTime())
         })
-        it('should roll the attempt count after 10 minutes locked out period', async () => {
+        it.skip('should roll the attempt count after 10 minutes locked out period', async () => {
             const clock = FakeTimers.install()
+            const user = await setUpUser({})
             const signInAttempt = getRepository().create({ user, count: 5, attemptedAt: new Date() })
             await getRepository().save(signInAttempt)
-            await expect(getService().isLockedOut(user)).resolves.toBe(true)
 
-            clock.tick('10:01') // one second
-
-            await expect(getService().isLockedOut(user)).resolves.toBe(false)
-
-            const signInAttemptAfter10m = await getRepository().findOne({ where: { user } })
-            expect(signInAttemptAfter10m.count).toBe(5)
+            clock.tick('10:01') // 10minutes and 1second
 
             await getService().updateSignInAttemptCountAfterWrongTry(user)
 
             const signInAttemptAfterCountUpdate = await getRepository().findOne({ where: { user } })
-            expect(signInAttemptAfterCountUpdate.count).toBe(1)
+
+            expect(signInAttemptAfterCountUpdate.count).toBe(5)
+
             clock.uninstall()
         })
     })
--- backend/src/users/sign-in-attempt/sign-in-attempt.service.ts
+++ backend/src/users/sign-in-attempt/sign-in-attempt.service.ts
@@ -4,21 +4,20 @@ import { SignInAttemptEntity } from './sign-in-attempt.entity'
 import { UserEntity } from '../user.entity'
 import { InjectRepository } from '@nestjs/typeorm'
 
-const TEN_MINUTES = 10*60*1000
+const TEN_MINUTES = 10 * 60 * 1000
 const ALLOWED_TRIES = 5
 
 @Injectable()
 export class SignInAttemptService {
-    private static lockoutPeriodReached = (attemptDate: Date):boolean => Date.now() - attemptDate!.getTime() > TEN_MINUTES
+    private static lockoutPeriodReached = (attemptDate: Date): boolean =>
+        Date.now() - attemptDate!.getTime() > TEN_MINUTES
     constructor(
         @InjectRepository(SignInAttemptEntity)
         private readonly repository: Repository<SignInAttemptEntity>,
-    ) {
-
-    }
+    ) {}
 
     private findOneByUser(user: UserEntity) {
-        return this.repository.findOne({where: { user }})
+        return this.repository.findOne({ where: { user } })
     }
 
     async isLockedOut(user: UserEntity) {
@@ -26,38 +25,33 @@ export class SignInAttemptService {
         if (!signInAttempt) {
             return false
         }
-        const {count, attemptedAt} = signInAttempt
-        if (count < ALLOWED_TRIES)
-            return false
+        const { count, attemptedAt } = signInAttempt
+        if (count < ALLOWED_TRIES) return false
 
-        if (SignInAttemptService.lockoutPeriodReached(attemptedAt!))
-            return false
+        if (SignInAttemptService.lockoutPeriodReached(attemptedAt!)) return false
 
         return true
     }
 
     async clearSignInAttemptCount(user: UserEntity) {
-        const signInAttempt = (await this.findOneByUser(user)) ?? this.repository.create({user})
-        return this.repository.save({...signInAttempt, count: 0, attemptedAt: new Date()})
-
+        const signInAttempt = (await this.findOneByUser(user)) ?? this.repository.create({ user })
+        return this.repository.save({ ...signInAttempt, count: 0, attemptedAt: new Date() })
     }
 
     async updateSignInAttemptCountAfterWrongTry(user: UserEntity) {
-        const signInAttempt = (await this.findOneByUser(user)) ?? this.repository.create({user})
-        const {count = 0, attemptedAt} = signInAttempt
+        const signInAttempt = (await this.findOneByUser(user)) ?? this.repository.create({ user })
+        const { count = 0, attemptedAt } = signInAttempt
         /*
             we give a user ALLOWED_TRIES tries to enter the  correct password
             if he fails to enter the correct time he needs to wait until lockout period is reached and we restart the counting
             we freeze the counter when he is still in the lockoutPeriod
          */
-        let newCount;
-        if (count < ALLOWED_TRIES)
-            newCount = count+1
-        else if (SignInAttemptService.lockoutPeriodReached(attemptedAt!))
-            newCount = 1
-        else
-            return signInAttempt
 
-        return this.repository.save({...signInAttempt, count: newCount, attemptedAt: new Date()})
+        let newCount
+        if (count < ALLOWED_TRIES) newCount = count + 1
+        else if (SignInAttemptService.lockoutPeriodReached(attemptedAt!)) newCount = 1
+        else return signInAttempt
+
+        return this.repository.save({ ...signInAttempt, count: newCount, attemptedAt: new Date() })
     }
 }
