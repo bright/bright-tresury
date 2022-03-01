a2bbeeedacffff39e56d25a8a3f7f028d91be783
Agnieszka Olszewska <agnieszka.olszewska@brightinventions.pl>
2022-03-01 21:08:41 +0100

    TREAS-401 Create docker composes for latest and current polkadot

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -4,14 +4,20 @@ source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/_shared.sh
 
 set -x
 
+compose=${PROJECT_DIR}/deploy/docker-compose.test.yml
+
+if [[ ${LATEST_POLKADOT} = true ]]; then
+  compose="${PROJECT_DIR}/deploy/docker-compose.test-latest-polkadot.yml"
+fi
+
 docker-compose \
-    --file ${PROJECT_DIR}/deploy/docker-compose.test.yml \
+    --file ${compose} \
     build --no-cache
 
 docker-compose \
-    --file ${PROJECT_DIR}/deploy/docker-compose.test.yml \
+    --file ${compose} \
     run backend-test-ci
 
 docker-compose \
-    --file ${PROJECT_DIR}/deploy/docker-compose.test.yml \
+    --file ${compose} \
     down --rmi 'all' --volumes  --remove-orphans
