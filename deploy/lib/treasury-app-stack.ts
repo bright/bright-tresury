c025e3da0983867116e35952fed6ebf936b8cece
Robert Koprowski <robert.koprowski@brightinventions.pl>
2022-04-25 14:42:39 +0200

    update polkadot node to v0.9.19

--- deploy/lib/treasury-app-stack.ts
+++ deploy/lib/treasury-app-stack.ts
@@ -14,7 +14,7 @@ export class TreasuryAppStack extends cdk.Stack {
             parameters: {
                 DeployEnv: deployEnv(),
                 AppImage: `${dockerRegistry()}treasury:${vcsVersion()}`,
-                PolkadotImage: `public.ecr.aws/x2h8r1m3/treasury/polkadot-dev:v0.9.18`,
+                PolkadotImage: `public.ecr.aws/x2h8r1m3/treasury/polkadot-dev:v0.9.19`,
             },
             templateFile: path.join(__dirname, '..', 'aws.template'),
         })
--- deploy/polkadot.Dockerfile
+++ deploy/polkadot.Dockerfile
@@ -1,1 +1,1 @@
-FROM public.ecr.aws/x2h8r1m3/treasury/polkadot-dev:v0.9.18
+FROM public.ecr.aws/x2h8r1m3/treasury/polkadot-dev:v0.9.19
